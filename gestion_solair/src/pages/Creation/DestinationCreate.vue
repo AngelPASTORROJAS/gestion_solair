6e4f6a1bcd6a9e5d3017e8b26b7b14791f80a5b3
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-01-17 20:11:22 +0100

    feat: input pour la création d'une destination

--- gestion_solair/src/pages/Creation/DestinationCreate.vue
+++ gestion_solair/src/pages/Creation/DestinationCreate.vue
@@ -1,16 +1,56 @@
 <template>
     <q-page class="q-pa-lg">
         <p class="sub-title">Destinations / Création</p>
+        <q-form @submit="onSubmit">
+            <div class="row bg-pink-4">
+                <q-list class="col-12" bordered padding>
+                    <q-expansion-item default-opened expand-separator label="Informations de la destination">
+                        <q-item>
+                            <div class="col-12">
+                                <InputCustom max="75" mention="Obligatoire" 
+                                    :rules="[(val) => !!val]"
+                                    label="Nom"
+                                    v-model="destination.nom"
+                                ></InputCustom>
+                                <InputCustom max="255" mention="Obligatoire" 
+                                    :rules="[(val) => !!val]"
+                                    label="Url de l'mage"
+                                    v-model="destination.url"
+                                ></InputCustom>
+                                <InputCustom max="255" mention="Obligatoire" 
+                                :rules="[(val) => !!val]"
+                                label="Description"
+                                v-model="destination.description"
+                                ></InputCustom>
+                                <InputCustom max="10" mention="Obligatoire" 
+                                    :rules="[(val) => !!val]"
+                                    label="Ville"
+                                    v-model="destination.ville"
+                                ></InputCustom>
+                            </div>
+                        </q-item>
+                    </q-expansion-item>
+                </q-list>
+            </div>
+        </q-form>
     </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import InputCustom from 'src/components/InputCustom.vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent ({
     name: 'Destinations-create',
     setup() {
-        return {};
+        const destination = ref({} as Destination)
+        return {
+            destination,
+        };
+    },
+
+    components: {
+        InputCustom,
     }
 })
 </script>
