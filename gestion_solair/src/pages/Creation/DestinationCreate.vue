cfc3f83af34ae54915c7f7b436604ad227a35267
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-01-17 18:37:31 +0100

    feat: création de la page de création d'une destination

---
+++ gestion_solair/src/pages/Creation/DestinationCreate.vue
@@ -0,0 +1,16 @@
+<template>
+    <q-page>
+        <p class="sub-title">Destinations / Création</p>
+    </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent ({
+    name: 'Destinations-create',
+    setup() {
+        return {};
+    }
+})
+</script>
