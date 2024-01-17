b1fb632b1355ea66750475eab8401abbebe9dfc5
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-01-17 10:22:00 +0100

    feat: ajout de la navigation sur le MainLayout.vue

--- gestion_solair/src/layouts/MainLayout.vue
+++ gestion_solair/src/layouts/MainLayout.vue
@@ -28,7 +28,7 @@
         <q-item-label
           header
         >
-          Essential Links
+          Gestion Solair
         </q-item-label>
 
         <EssentialLink
@@ -51,46 +51,22 @@ import EssentialLink from 'components/EssentialLink.vue';
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: 'Critères',
+    caption: 'Criteres',
+    icon: 'filter_list',
+    link: '#/Criteres'
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+    title: 'Destinations',
+    caption: 'Destinations',
+    icon: 'travel_explore',
+    link: '#/Destinations'
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+    title: 'Utilisateurs',
+    caption: 'Utilisateurs',
+    icon: 'groups',
+    link: '#/Utilisateurs'
   }
 ];
 
