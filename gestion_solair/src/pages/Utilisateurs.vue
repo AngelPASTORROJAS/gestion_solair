08ae5252c0dfaba658235611d566de65eca11444
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-01-17 14:39:18 +0100

    feat: ajout du titre et bouton de création d'utilisateur dans Utilisateurs.vue

--- gestion_solair/src/pages/Utilisateurs.vue
+++ gestion_solair/src/pages/Utilisateurs.vue
@@ -1,5 +1,10 @@
 <template>
-    <q-page></q-page>
+  <q-page class="q-pa-lg">
+    <div class="row justify-between">
+      <p class="sub-title">Utilisateurs</p>
+      <q-btn class="bouton-default" label="Créer" :to="{ name: 'utilisateur-create' }"></q-btn>
+    </div>
+  </q-page>
 </template>
 
 <script lang="ts">
